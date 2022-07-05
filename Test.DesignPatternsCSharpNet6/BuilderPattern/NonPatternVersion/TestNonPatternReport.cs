f244ab1d08dc68abe23cdc8ec3f6b62a7bd48499
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-07-05 11:53:21 -0500

    Added Builder design pattern code

---
+++ Test.DesignPatternsCSharpNet6/BuilderPattern/NonPatternVersion/TestNonPatternReport.cs
@@ -0,0 +1,32 @@
+﻿using DesignPatternsCSharpNet6.Builder.NonPatternVersion;
+
+namespace Test.DesignPatternsCSharpNet6.BuilderPattern.NonPatternVersion;
+
+public class TestNonPatternReport
+{
+    [Fact]
+    public void Test_BuildReports()
+    {
+        DateTime now = DateTime.UtcNow;
+
+        Report currentMonthTaxReport =
+            new Report(new DateTime(now.Year, now.Month, 1),
+                new DateTime(now.Year, now.Month, 1).AddMonths(1).AddSeconds(-1),
+                false, true, Report.SortingMethod.ByTaxCategory);
+
+        Report currentYearTaxReport =
+            new Report(new DateTime(now.Year, 1, 1),
+                new DateTime(now.Year, 12, 31),
+                false, true, Report.SortingMethod.ByTaxCategory);
+
+        Report currentMonthCommissionReport =
+            new Report(new DateTime(now.Year, now.Month, 1),
+                new DateTime(now.Year, now.Month, 1).AddMonths(1).AddSeconds(-1),
+                false, false, Report.SortingMethod.BySalesperson);
+
+        Report currentYearCommissionReport =
+            new Report(new DateTime(now.Year, 1, 1),
+                new DateTime(now.Year, 12, 31),
+                false, false, Report.SortingMethod.BySalesperson);
+    }
+}
---
+++ Test.DesignPatternsCSharpNet6/BuilderPattern/PatternVersion/TestPatternReport.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatternsCSharpNet6.Builder.PatternVersion;
+
+namespace Test.DesignPatternsCSharpNet6.BuilderPattern.PatternVersion;
+
+public class TestPatternReport
+{
+    [Fact]
+    public void Test_BuildReports()
+    {
+        Report currentMonthTaxReport =
+            ReportBuilder.CreateMonthTaxReport(7, 2022);
+
+        Report currentYearTaxReport =
+            ReportBuilder.CreateYearTaxReport(2022);
+
+        Report currentMonthCommissionReport =
+            ReportBuilder.CreateMonthCommissionReport(7, 2022);
+
+        Report currentYearCommissionReport =
+            ReportBuilder.CreateYearCommissionReport(2022);
+    }
+}

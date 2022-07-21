77cfd7bafdf476e2f1d681c60d6248e8ed21f9ec
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-07-21 09:32:05 -0500

    Added Active Record and Data Mapper

---
+++ DesignPatternsCSharpNet6/DataMapper/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatternsCSharpNet6.DataMapper;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsPremiumMember { get; set; }
+
+    public Customer(int id, string name, bool isPremiumMember)
+    {
+        Id = id;
+        Name = name;
+        IsPremiumMember = isPremiumMember;
+    }
+}
